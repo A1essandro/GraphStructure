fc5985c68ad4ade1568984a5d29ca09d92124775
A1essandro <alexander.yermakov.1990@gmail.com>
2018-06-05 01:00:59 +0400

    Fix deploy commands

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ApiKey=$1
 
-dotnet nuget pack ./GraphStructure -v=n
-dotnet nuget push ./GraphStructure.1.0.0.nupkg -k $ApiKey -s https://api.nuget.org/v3/index.json
+dotnet pack GraphStructure -v=n
+dotnet nuget push GraphStructure.0.1.0-alpha.nupkg -k $ApiKey -s https://api.nuget.org/v3/index.json
