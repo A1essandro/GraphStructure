c66b531666d9e7892ad0b20a32baa66694aeba78
A1essandro <alexander.yermakov.1990@gmail.com>
2018-06-05 01:08:00 +0400

    Fix path to nupkg

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ApiKey=$1
 
 dotnet pack GraphStructure -v=n
-dotnet nuget push GraphStructure.0.1.0-alpha.nupkg -k $ApiKey -s https://api.nuget.org/v3/index.json
+dotnet nuget push ./GraphStructure/bin/Debug/GraphStructure.0.1.0-alpha.nupkg -k $ApiKey -s https://api.nuget.org/v3/index.json
