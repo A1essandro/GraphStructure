b5cd7a88a2def4c6f62f5045c8f813e791007610
A1essandro <alexander.yermakov.1990@gmail.com>
2018-06-05 19:51:29 +0400

    Fix path to package for deploy (nuget push)

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ApiKey=$1
 Version=$2
 
-dotnet pack GraphStructure -v=n /p:PackageVersion=$Version
-dotnet nuget push GraphStructure.*.nupkg -k $ApiKey -s https://api.nuget.org/v3/index.json
+dotnet pack GraphStructure -v=n /p:PackageVersion=$Version /p:Configuration=Release
+dotnet nuget push ./GraphStructure/bin/Release/GraphStructure.*.nupkg -k $ApiKey -s https://api.nuget.org/v3/index.json
